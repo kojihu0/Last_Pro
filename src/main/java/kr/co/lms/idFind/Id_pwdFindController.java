e726cf2c7c9613bb5102ee50226af1a291e3280c
Hyeonji-Lee <hyperlhg@gmail.com>
2020-07-07 18:12:13 +0900

    패스워드 재생성 시 사용불가능한 특수문자 삭제

--- src/main/java/kr/co/lms/idFind/Id_pwdFindController.java
+++ src/main/java/kr/co/lms/idFind/Id_pwdFindController.java
@@ -106,7 +106,7 @@ public class Id_pwdFindController {
 		char[] charset = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' ,'l', 'm',
 						'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 		char[] charsetNum = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
-		char[] charsetSp = new char[]{'!', '@', '#', '$', '%', '^', '&', '-', '+', '='};
+		char[] charsetSp = new char[]{'!', '@', '#', '$', '%', '&'};
 		StringBuffer newPwd = new StringBuffer();
 		for(int i=0; i<10; i++) {
 			int idx = 0;
